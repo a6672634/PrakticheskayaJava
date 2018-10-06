aa617f4bccb468e023a07ec403edad3a39b1153e
qa <xbx@jn.dd>
2018-10-06 20:45:05 +0300

    Kusti

---
+++ src/org/itstep/qa/lesson/Kust.java
@@ -0,0 +1,14 @@
+package org.itstep.qa.lesson;
+
+public class Kust {
+    private int XX;
+    private int YY;
+    private String color;
+
+    public void Kust1(int XX, int YY, String color) {
+        this.XX = XX;
+        this.YY = YY;
+        this.color = color;
+    }
+
+}
---
+++ src/org/itstep/qa/runner/RunnerMouse.java
@@ -0,0 +1,12 @@
+package org.itstep.qa.runner;
+
+import org.itstep.qa.lesson.Kust;
+import org.itstep.qa.lesson.Mouse;
+
+public class RunnerMouse {
+    public static void main(String[] args) {
+        Mouse mouse =new Mouse();
+        Kust kust = new Kust();
+
+    }
+}
